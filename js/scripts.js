506134cf3a5f8e01081827d9ff2db4db4f88f924
ines.sousa <ines.barrs@outlook.pt>
2024-06-26 00:04:46 +0100

    alteracoes 26-06

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById('contact-form');
+const form = document.getElementById('contact-form');
+
+if (form) {
+
     const successMessage = document.getElementById('success-message');
+
+    const subtmitButton = document.getElementById('submitButton');
 
     form.addEventListener('submit', function (event) {
         event.preventDefault(); // Impede o envio real do formulário
@@ -8,12 +12,40 @@ document.addEventListener('DOMContentLoaded', function () {
         // Exibe a mensagem de sucesso
         successMessage.classList.remove('hidden');
 
+        subtmitButton.disabled = true;
+
         // Limpa os campos do formulário
         form.reset();
 
-        // Oculta a mensagem de sucesso após alguns segundos (opcional)
+        // Oculta a mensagem e botao
         setTimeout(() => {
-            successMessage.classList.add('hidden');
+            subtmitButton.disabled = false;
+
+            successMessage.classList.add('hidden'); 
         }, 5000); // 5000 ms = 5 segundos
     });
+}
+
+
+// JSCRIPT
+const img = document.getElementById('cardio2');
+
+if (img) {
+img.addEventListener('click', function () {
+    alert("click.element")
+
 });
+};
+
+const sobre = document.getElementById('sobre');
+
+if (sobre) {
+    sobre.addEventListener('mouseenter', function () {
+        sobre.style.transform = "scale(1.5)";
+    });
+
+    sobre.addEventListener('mouseleave', function () {
+        sobre.style.transform = "scale(1)";
+    });
+};
+
